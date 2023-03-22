00b90cf72ad6fddff5e13c024b1472bfd729c431
erebollo@capgemini.com <erebollo@capgemini.com>
2023-03-22 19:51:40 +0100

    dynamic y-axis

--- js/histogram.js
+++ js/histogram.js
@@ -3,6 +3,7 @@ let n_bins;
 let sims = [];
 let domain = [0,0];
 let max_prob = 1;
+let h_ref;
 
 let summa = 0;
 let mean = 0;
@@ -58,21 +59,22 @@ function message(e) {
     let value = parseFloat(json.value);
     iter_text.text("iter " + iter).attr("x", width-margin.right).attr("y", 2*margin.top);
     if (sims.length == 0) {
-      domain = [value - (value/10), value + (value/10)];
+      href = value/10;
+      domain = [value - href, value + href];
       x = d3.scaleLinear()
           .domain(domain)
           .range([margin.left, width - margin.right]);
       axis[0].call(d3.axisBottom(x));
     } else if (value <= domain[0]) {
       console.log("se va por la izquierda.")
-      if (value>0) domain[0] = value - (value/10);
-      else domain[0] = value + (value/10);
+      if (value>0) domain[0] = value - href;
+      else domain[0] = value + href;
       x.domain(domain);
       axis[0].call(d3.axisBottom(x));
     } else if (value >= domain[1]) {
       console.log("se va por la derecha.")
-      if (value>0) domain[1] = value + (value/10);
-      else domain[1] = value - (value/10);
+      if (value>0) domain[1] = value + href;
+      else domain[1] = value - href;
       x.domain(domain);
       axis[0].call(d3.axisBottom(x));
     }
@@ -179,7 +181,7 @@ function resize() {
   axis[1].attr("transform", `translate(${margin.left},0)`);
   axis[1].call(d3.axisLeft(y));
   h_lines.forEach((l,i) => {
-    let h = y((i+1)/10);
+    let h = y(max_prob/10*(i+1)/10);
     l.attr("x1", margin.left).attr("x2", width-margin.right)
     .attr("y1", h).attr("y2", h);
   });
