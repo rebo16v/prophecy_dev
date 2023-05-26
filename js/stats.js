1a3fce5690438101cc58670eae5a308cff5af1fe
erebollo@capgemini.com <erebollo@capgemini.com>
2023-05-26 21:11:01 +0200

    stats

---
+++ js/stats.js
@@ -0,0 +1,37 @@
+function mean = arr => {
+  const sum = arr.reduce((a, b) => a + b, 0);
+  const avg = (sum / arr.length) || 0;
+}
+
+function stdev = arr => {
+  const n = arr.length;
+  const mean = arr.reduce((a, b) => a + b) / n;
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
+}
+
+const median = arr => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
+
+function mode arr => {
+  let object = {}
+  for (let i = 0; i < arr.length; i++) {
+    if (object[arr[i]]) {
+      object[arr[i]] += 1;
+    } else {
+      object[arr[i]] = 1;
+    }
+  }
+  let biggestValue = -1;
+  let biggestValuesKey = -1;
+  Object.keys(object).forEach(key => {
+    let value = object[key]
+    if (value > biggestValue) {
+      biggestValue = value
+      biggestValuesKey = key
+    }
+  });
+  return biggestValuesKey;
+}
