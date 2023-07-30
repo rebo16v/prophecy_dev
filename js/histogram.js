c8c02bb93ee642d1fd1ab672fba6d9c5e553c407
rebo16v <rebo16v@gmail.com>
2023-07-30 22:40:25 +0200

    added stats

--- js/histogram.js
+++ js/histogram.js
@@ -238,7 +238,7 @@ function resize() {
       });
 	if (mdown != -1 && mup != -1) {
 		inter_line
-			.attr("x", x(Math.min(mdown, mup))).attr("width", x(Math.abs(mdown-mup)))
+			.attr("x", x(Math.min(mdown, mup))).attr("width", Math.abs(x(mdown)-x(mup)))
 			.attr("y", margin.top).attr("height", height-margin.top-margin.bottom);
 		inter_text.attr("x", Math.max(mdown,mup)).attr("y", margin.top);		
 		}
